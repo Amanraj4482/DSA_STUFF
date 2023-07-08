01003899f7aa07b0fc44810e0f91f596e3988b35
Amancore__ <114256199+Amanraj4482@users.noreply.github.com>
2023-07-08 16:29:50 +0530

    Update Insertion_c.c

--- Insertion_c.c
+++ Insertion_c.c
@@ -10,6 +10,13 @@ struct node* create_node(int value) {
 	new_node->next = NULL;
 	return new_node;
 }
+void printing(struct node *head) {
+	struct node* current = head;
+	while (current != NULL) {
+		printf("%d ", current->data);
+		current = current->next;
+	}
+}
 void insert_end(struct node**head, int value) {
 	struct node* new_node = create_node(value);
 	if (*head == NULL) {
@@ -29,22 +36,55 @@ void insert_beg(struct node**head, int value) {
 	*head = new_node;
 }
 void insert_pos(struct node**head, int value, int pos) {
+	if (pos < 0) return ;
+	if (pos == 0) { //if the index is equal to the zero
+		insert_beg(head, value);
+		return ;
+	}
 	struct node* new_node = create_node(value);
 	struct node* current = *head;
+	//if the index is greater than the zero --------------
+	int i = 2;
+	while (i < pos) { /*(*head!NULL&&count<index-1)*/ //empty list
+		current = current->next;
+		i++;//--pos;
+	}
+	//if (pos==NULL)
+	new_node->next = current->next;
+	current->next = new_node;
+}
+void delete_beg(struct node** head) {
+	if (*head == NULL) {
+		printf("LIST IS EMPTY");
+		return;
+	}
+	struct node* current = *head;
+	*head = current->next;
+	free(current);
+}
+void delete_end(struct node** head) {//it needs two pointers
+	struct node* current = *head;
+	while (current->next->next != NULL) {
+		current = current->next;
+	}
+	if (current == *head) {
+		*head = NULL;
+		free(current);
+		return;
+	}
+	current->next = NULL;
+	free(current->next);
+}
+void delete_pos(struct node **head, int pos) {
+	struct node* current = *head, *temp;
 	int i = 2;
 	while (i < pos) {
 		current = current->next;
 		i++;
 	}
-	new_node->next = current->next;
-	current->next = new_node;
-}
-void printing(struct node *head) {
-	struct node* current = head;
-	while (current != NULL) {
-		printf("%d ", current->data);
-		current = current->next;
-	}
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
 }
 int main() {
 	struct node* head = NULL;
@@ -60,6 +100,19 @@ int main() {
 	int data = 60, pos = 2;
 	insert_pos(&head, data, pos);
 
+//delete from beginning
+	delete_beg(&head);
+// //delete from ending
+	delete_end(&head);
+//delete from specified position
+	delete_pos(&head, pos); //pos=2;
+
 	printing(head);
 }
 // 50 60 40 10 20 30
+//delete from beg=60 40 10 20 30
+//delete from end=60 40 10 20
+//delete form the pos=60 10 20
+
+
+

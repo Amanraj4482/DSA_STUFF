054bfde1393f75b2106cef4e6c983e2cf0abc7a1
Amancore__ <114256199+Amanraj4482@users.noreply.github.com>
2023-07-22 15:41:41 +0530

    Update Doubly_linked_list.cpp

--- Doubly_linked_list.cpp
+++ Doubly_linked_list.cpp
@@ -1,5 +1,3 @@
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -18,28 +16,36 @@ struct node* create_node(int value) {
 	return new_node;
 }
 
-//inserting the new node to the beg
 void insert_at_beg(struct node** head, int value) {
 //create a new node
 	struct node* new_node = create_node(value);
 	new_node->next = *head;
 	*head = new_node;
 }
-void insert_at_end(struct node** head, int value) {
+void insert_at_end(struct node** head, struct node** tail, int value) {
 	struct node* new_node = create_node(value);
 	if (*head == NULL) {
-		*head = new_node;
+		*tail = * head = new_node; //head poiting to the new_node
 		return;
 	}
-	//if the head is filled
-	struct node* current = *head;
-	while (current->next != NULL) {
-		current = current->next;
-	}
-	current->next = new_node;//forward most link has been updated
-	new_node->prev = current;//updating the new_node -> prev will be the current
-	current = new_node;
+	(*tail)->next = new_node;
+	new_node->prev = *tail;
+	*tail = new_node;
+}
 
+void insert_at_pos(struct node** head, struct node** tail, int value, int index) {
+	struct node* new_node = create_node(value);
+	struct node* current = *head;
+	int i = 0;
+	while (i < index - 1) {
+		current = current->next;
+		i++;
+	}
+	//i have the index at 2
+	new_node->prev = current;
+	new_node->next = current->next;
+	current->next = new_node;
+	new_node->next->prev = new_node;
 }
 void printing(struct node* head) {
 	struct node* current = head;
@@ -49,11 +55,18 @@ void printing(struct node* head) {
 	}
 }
 int main() {
-	struct node* head = NULL;
-	insert_at_beg(&head, 23);
-	insert_at_end(&head, 19);
-	insert_at_end(&head, 21);
-	insert_at_end(&head, 22);
+	struct node* head  = NULL;
+	struct node* tail = NULL;
+	insert_at_end(&head, &tail, 19);
+	insert_at_end(&head, &tail, 20);
+	insert_at_end(&head, &tail, 21);
+	insert_at_beg(&head,  22);
+	insert_at_beg(&head,  24);
+
+
+	int index = 2;
+	insert_at_pos(&head, &tail, 60, index);
 	printing(head);
 	return 0;
 }
+

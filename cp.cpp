7d849565c86211c592e35516b3ca53d40c750a49
Amancore__ <114256199+Amanraj4482@users.noreply.github.com>
2024-03-13 18:25:33 +0530

    Create cp.cpp

---
+++ cp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define p(...);
+#endif
+#define endl "\n"
+#define int long long
+#define ull unsigned long long
+#define L(_,a,n) for(int _= a; _< (n); ++_)
+#define L1(_,a,n) for(int _= a; _<=(n); ++_)
+
+void solve() {
+
+}
+signed main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	solve();
+}

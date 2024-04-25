62cc84b5e5d54cb4c6a2665ba501de5c44db6ea6
Amancore__ <114256199+Amanraj4482@users.noreply.github.com>
2024-04-25 12:13:08 +0530

    Create nf.cpp

---
+++ SUBLIME_TEMPLATE/nf.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include <D:/deb.hpp>
+#else
+#define p(...);
+#define show(x);
+#define bl
+#endif
+#define int long long
+#define L(i,x,n) for(int i=(x);i<(n);++i)
+#define R(i,n,x) for(int i=(n);i>=(x);--i)
+
+void solve() {
+
+}
+signed main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
+#endif
+	int t = 1; cin >> t;
+	L(i, 1, t + 1) {show(i); solve(); bl;}
+}
